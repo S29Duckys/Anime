c6101041246e35810b69e2fb02a670c8bedc8775
Olivier NEUGNOT <olivier.ngnt@gmail.com>
2026-02-28 21:26:48 +0100

    feat: set InnoDB engine for multiple migration tables to enhance performance and support foreign key constraints

--- database/migrations/2026_02_27_204513_add_info_anime_table.php
+++ database/migrations/2026_02_27_204513_add_info_anime_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('info_anime', function (Blueprint $table) {
+            $table->engine = "InnoDB";
             $table->id();
             $table->string('title');
             $table->string('original_title');
