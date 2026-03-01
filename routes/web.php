aa8d114db88d5c13a0050977a1a30ef426570e0a
s29duckys <nbarasraso@edenschool.fr>
2026-03-01 20:53:35 +0100

    refactor: remove unused models and fix AnimeController reference in routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\SaisonsController;
 use App\Http\Controllers\MalisteController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AccueilController;
+use App\Http\Controllers\AnimeController;
 
 
 Route::get('/cgu', function () {
@@ -28,7 +29,7 @@ Route::controller(UsersController::class)->group(function () {
     Route::get('/settings','settings')->name('settings');
 });
 
-Route::get('/anime/{id}', [AnimeControlleur::class, 'show'])->name('anime.show');
+Route::get('/anime/{id}', [AnimeController::class, 'show'])->name('anime.show');
 Route::get('/catalogue', [CatalogueController::class, 'catalogue'])->name('catalogue');
 Route::get('/tendances', [TendancesController::class, 'tendances'])->name('tendances');
 Route::get('/genres',[GenreController::class,'genres'])->name('genres');
